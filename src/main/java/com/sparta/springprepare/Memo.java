e57d027206d5e68ad5ca041760a19a6727a22245
R.jisoo <jisuryu0527@gmail.com>
2024-09-29 03:38:39 +0900

    Feat: studying lombok & application

--- src/main/java/com/sparta/springprepare/Memo.java
+++ src/main/java/com/sparta/springprepare/Memo.java
@@ -1,4 +1,23 @@
 package com.sparta.springprepare;
 
+import lombok.*;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+// 기본 생성자
+@NoArgsConstructor
 public class Memo {
+    private String username;
+    private String contents;
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Memo memo = new Memo("Robbert","내용");
+        memo.setUsername("Jack");
+        System.out.println(memo.getUsername());
+    }
 }
